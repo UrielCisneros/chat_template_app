b193e4905a754f7f05fd72af70b218e14a684d98
Uriel Cisneros <urielcisne96@gmail.com>
2023-08-02 11:48:01 -0600

    add control scroll in chat

--- lib/providers/chat_provider.dart
+++ lib/providers/chat_provider.dart
@@ -2,14 +2,26 @@ import 'package:chat_template_app/domain/entities/message.dart';
 import 'package:flutter/material.dart';
 
 class ChatProvider extends ChangeNotifier {
+  final ScrollController chatScrollController = ScrollController();
+
   List<Message> messagesList = [
     Message(text: "Hola", fromWho: FromWho.me),
     Message(text: "Holaaa", fromWho: FromWho.hers)
   ];
 
   Future<void> sendMessage(String text) async {
+    if (text.isEmpty) return;
     final newMessage = Message(text: text, fromWho: FromWho.me);
     messagesList.add(newMessage);
     notifyListeners();
+    moveScrollToBottom();
+  }
+
+  Future<void> moveScrollToBottom() async {
+    await Future.delayed(const Duration(microseconds: 100));
+    chatScrollController.animateTo(
+        chatScrollController.position.maxScrollExtent,
+        duration: const Duration(microseconds: 300),
+        curve: Curves.easeOut);
   }
 }
